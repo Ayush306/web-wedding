15fca7871d5d485d8451270b3a4e429fcfbcc18b
unknown <ayushtomar306@gmail.com>
2024-04-09 13:49:58 +0530

    final

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -25,7 +25,7 @@ export class HomeComponent implements AfterViewInit,OnInit{
                 code: 'f230fh0g3',
                 name: 'Bamboo Watch',
                 description: 'Product Description',
-                image: 'wed-img-5.jpg',
+                image: 'wed-img-1.jpg',
                 price: 65,
                 category: 'Accessories',
                 quantity: 24,
@@ -37,7 +37,7 @@ export class HomeComponent implements AfterViewInit,OnInit{
                 code: 'nvklal433',
                 name: 'Black Watch',
                 description: 'Product Description',
-                image: 'wed-img-4.jpg',
+                image: 'wed-img-2.jpg',
                 price: 72,
                 category: 'Accessories',
                 quantity: 61,
@@ -49,13 +49,25 @@ export class HomeComponent implements AfterViewInit,OnInit{
                 code: 'zz21cz3c1',
                 name: 'Blue Band',
                 description: 'Product Description',
-                image: 'wed-img-3.jpg',
+                image: 'wed-img-11.jpg',
                 price: 79,
                 category: 'Fitness',
                 quantity: 2,
                 inventoryStatus: 'LOWSTOCK',
                 rating: 3
             },
+            {
+              id: '1002',
+              code: 'zz21cz3c1',
+              name: 'Blue Band',
+              description: 'Product Description',
+              image: 'wed-img-12.jpg',
+              price: 79,
+              category: 'Fitness',
+              quantity: 2,
+              inventoryStatus: 'LOWSTOCK',
+              rating: 3
+          },
             ]
         ;
     this.responsiveOptions = [
